4b1f37c852640a8feb58cc6a15ccf4d2e967e26a
Incognito Sudo <gfranco.mg@gmail.com>
2024-06-09 16:39:48 -0700

    Add files via upload

---
+++ my_pkg/convert.py
@@ -0,0 +1,9 @@
+#define 3 functions
+def lb_to_oz(lb):   #takes number of pounds to number of ounces
+    return (lb *16)
+
+def oz_to_lb(oz):
+    return (oz / 16)  #converts ounces to pounds
+
+def ft_to_in(ft):
+    return (ft*12)   #converts feet to inches
