c7e48e01241d559412de99d7b1d0d6d2368d9bd3
Incognito Sudo <gfranco.mg@gmail.com>
2024-06-09 21:48:30 -0700

    Add files via upload

---
+++ builtin_functions.py
@@ -0,0 +1,17 @@
+print("Math Functions")
+
+abs_int = abs(-1)   #declare a variable abs_int assigning a value of function name caled abs aka absolute. Returns any number to positive number
+print(abs_int)
+
+int_to_float = float(100)   #turn anynumber to any float value.
+print(int_to_float)
+
+flaot_to_int = int(1.23)  #conver any value into integer.
+print(flaot_to_int)  #or print(int(1.23)) if you dont want to recall variable again.
+
+
+print(int(1.23))
+print(max(1,2, -5, 10, 0))   #prints 10
+print(min(1,2,-5,10,0))   #prints -5
+print(pow(2,3))   #2 to power of 3
+print(round(51.5))
